b12dce29af3ea512a62c8845153f9cc04d555bf9
beretk <137759605+beretk@users.noreply.github.com>
2023-07-06 11:27:45 +0900

    Add files via upload

---
+++ source/01_Java/ch02_variable/src/com/lec/ex/VarEx06.java
@@ -0,0 +1,14 @@
+package com.lec.ex;
+// 기본데이터타입(기초테이터타입; 소문자 시작) vs. 참조데이터타입(객체데이터타입; 대문자시작)
+public class VarEx06 {
+	public static void main(String[] args) {
+		// 기본데이터타입
+		int i; // 4byte
+		i = 10;
+		// 참조데이터타입(객체데이터타입)
+		String name;
+		name = "KIM";
+		name = "김수한무";
+		System.out.println("name = " + name);
+	}
+}
