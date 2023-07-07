b37df511cde490a5c37ca87ea8240a74ecdfa6b5
beretk <beretk@naver.com>
2023-07-07 13:16:49 +0900

    ch02_variable

--- source/test/Personal Ex/src/com/lec/Ex/Ex01.java
+++
@@ -1,9 +0,0 @@
-package com.lec.Ex;
-
-public class Ex01 {
-	public static void main(String[] args) {
-		int age = 57;
-		String name = "±è";
-		System.out.println("Hellow" + age + "»ì" + name + "¾¾");
-	}
-}
